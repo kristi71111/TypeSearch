a7cf934adfd39c57f6915cab74f88422613a5abf
kristi71111 <kristjan.gorinek20@gmail.com>
2023-12-07 20:05:01 +0100

    1.16.5 port

--- README.md
+++
@@ -1,7 +0,0 @@
-# TypeSearch
-- A very simple mod that allows you to search @type in the pc so for example @fire would return all Pokemon that are fire type.
-- Requires Pixelmon Reforged
-- Server side only!
-
-# Support
-- [**Click here to join my support discord.**](https://discord.gg/x53dk93Xsk)
--- src/main/java/kristi71111/typesearch/mixins/ServerQueryPCPacketMixin.java
+++ src/main/java/kristi71111/typesearch/mixins/ServerQueryPCPacketMixin.java
@@ -1,10 +1,9 @@
 package kristi71111.typesearch.mixins;
 
+import com.pixelmonmod.pixelmon.api.pokemon.Element;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.api.storage.PokemonStorage;
-import com.pixelmonmod.pixelmon.comm.packetHandlers.clientStorage.newStorage.pc.ServerQueryPC;
-import com.pixelmonmod.pixelmon.entities.pixelmon.stats.BaseStats;
-import com.pixelmonmod.pixelmon.enums.EnumType;
+import com.pixelmonmod.pixelmon.comm.packetHandlers.clientStorage.newStorage.pc.ServerQueryPCPacket;
 import org.apache.commons.lang3.text.WordUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,37 +14,36 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@Mixin(value = ServerQueryPC.class)
-public abstract class MixinServerQueryPC {
+@Mixin(value = ServerQueryPCPacket.class)
+public abstract class ServerQueryPCPacketMixin {
     /**
      * @author kristi71111
      * @reason Add option to search @type
      */
     @Inject(method = "search", at = @At("HEAD"), remap = false, cancellable = true)
-    private static void search(PokemonStorage storage, String query, CallbackInfoReturnable<List<Pokemon>> cir) {
+    public void search(PokemonStorage storage, String query, CallbackInfoReturnable<List<Pokemon>> cir) {
         //Length check so that we don't check the @ before it can even possibly match anything at all.
         if (query.length() > 3 && Character.toString(query.charAt(0)).equals("@")) {
             final String getType = WordUtils.capitalize(query.substring(1));
-            List<Pokemon> matches = new ArrayList<>();
-            final EnumType searchedType = EnumType.parseOrNull(getType);
-            if (searchedType == null) {
+            final Element searchedElement = Element.parseOrNull(getType);
+            if (searchedElement == null) {
                 cir.setReturnValue(Collections.emptyList());
             }
-            Pokemon[] all = storage.getAll();
-            for (int i = 0, allLength = all.length; i < allLength; i++) {
-                Pokemon pokemon = all[i];
-                if (pokemon == null || pokemon.isEgg()) {
+            final List<Pokemon> matches = new ArrayList<>();
+            final Pokemon[] pokemon = storage.getAll();
+            for (int i = 0; i < pokemon.length; ++i) {
+                Pokemon pokemonActual = pokemon[i];
+                if (pokemonActual == null || pokemonActual.isEgg()) {
                     continue;
                 }
-                BaseStats stats = pokemon.getBaseStats();
-                EnumType type1 = stats.getType1();
-                EnumType type2 = stats.getType2();
-                if (type1.equals(searchedType) || (type2 != null && type2.equals(searchedType))) {
-                    matches.add(pokemon);
+                for (Element element : pokemonActual.getForm().getTypes()) {
+                    if (element.getName().equals(getType)) {
+                        matches.add(pokemonActual);
+                        break;
+                    }
                 }
             }
             cir.setReturnValue(matches);
         }
     }
 }
-
